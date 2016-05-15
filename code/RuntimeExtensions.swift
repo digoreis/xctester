07845f17a8881f0506cc1b7116a1694dda3d103f
Boris Bügling <boris@icculus.org>
2016-05-15 16:36:52 +0200

    Update for Swift 2.2

--- code/RuntimeExtensions.swift
+++ code/RuntimeExtensions.swift
@@ -1,14 +1,14 @@
 import Foundation
 import ObjectiveC
 
-@asmname("objc_msgSend") func sendPerformSelector(_:NSObject, _:Selector, _:Selector) -> AnyObject!;
+@_silgen_name("objc_msgSend") func sendPerformSelector(_:NSObject, _:Selector, _:Selector) -> AnyObject!;
 
 let msgSend_block : @convention(block) (sself: NSObject, aSelector: Selector) -> AnyObject! = { (sself, aSelector) -> (AnyObject!) in
-    return sendPerformSelector(sself, "performSelector:", aSelector)
+    return sendPerformSelector(sself, #selector(NSObjectProtocol.performSelector(_:)), aSelector)
 }
 
 let msgSend_IMP = imp_implementationWithBlock(unsafeBitCast(msgSend_block, AnyObject.self))
-let method = class_getInstanceMethod(NSObject.self, "performSelector:")
+let method = class_getInstanceMethod(NSObject.self, #selector(NSObjectProtocol.performSelector(_:)))
 let msgSend_old_IMP = method_setImplementation(method, msgSend_IMP)
 
 func class_getSubclasses(parentClass: AnyClass) -> [AnyClass] {
--- code/XCTestCaseExtensions.swift
+++ code/XCTestCaseExtensions.swift
@@ -45,14 +45,20 @@ let recordUnexpectedFailure_block : @convention(block) (sself: XCTestCase, descr
     sself.records.append(Failure(description: truncatedDescription, filePath: nil, lineNumber: 0, expected: false))
 }
 
+extension XCTestCase {
+    func _recordUnexpectedFailureWithDescription(desc: String, exception: NSException) {
+        fatalError("should never be called")
+    }
+}
+
 class LolSwift: NSObject {
     override class func initialize() {
         let recordFailure_IMP = imp_implementationWithBlock(unsafeBitCast(recordFailure_block, AnyObject.self))
-        let recordFailure_method = class_getInstanceMethod(XCTestCase.self, "recordFailureWithDescription:inFile:atLine:expected:")
+        let recordFailure_method = class_getInstanceMethod(XCTestCase.self, #selector(XCTestCase.recordFailureWithDescription(_:inFile:atLine:expected:)))
         let _ = method_setImplementation(recordFailure_method, recordFailure_IMP)
 
         let recordUnexpectedFailure_IMP = imp_implementationWithBlock(unsafeBitCast(recordUnexpectedFailure_block, AnyObject.self))
-        let recordUnexpectedFailure_method = class_getInstanceMethod(XCTestCase.self, "_recordUnexpectedFailureWithDescription:exception:")
+        let recordUnexpectedFailure_method = class_getInstanceMethod(XCTestCase.self, #selector(XCTestCase._recordUnexpectedFailureWithDescription(_:exception:)))
         let _ = method_setImplementation(recordUnexpectedFailure_method, recordUnexpectedFailure_IMP)
     }
 }
@@ -86,20 +92,23 @@ public func XCTestRunAll() -> Bool {
         let suites = (testRun.test as! XCTestSuite).tests
 
         for suite in suites {
-            let testCaseName = suite.name
-            print(testCaseName + "\n")
+            if let suiteName = suite.name {
+                print(suiteName + "\n")
+            }
 
             for test in (suite as! XCTestSuite).tests {
                 let testCase = test as! XCTestCase
 
                 let status = testCase.success ? "✅" : "❌"
-                print("\(status)  \(testCase.name)")
+                if let testCaseName = testCase.name {
+                    print("\(status)  \(testCaseName)")
+                }
 
                 if (testCase.success) {
                     continue
                 }
 
-                failureCount++
+                failureCount += 1
 
                 for failure in testCase.records {
                     print("\t\(failure)")
